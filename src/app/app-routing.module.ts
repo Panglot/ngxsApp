9faad7d7b32555c2d78943f0a47b49335fed4e54
dobromirbogatev@gmail.com <dobromirbogatev@gmail.com>
2022-03-24 16:21:12 +0200

    feat: update of structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { Test1Module } from './modules/test1/test1.module';
+import { Test1Component } from './modules/test1/test1.component';
+import { HomeModule } from './modules/home/home.module';
+import { HomeComponent } from './modules/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test-page-1', component: Test1Component }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeModule,
+    Test1Module
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
